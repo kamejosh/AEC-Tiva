71e8b48fb8a6feaa61ce6b802904b4dd7266a1af
kamejosh <josh@bregenznet.at>
2017-04-15 16:19:11 +0200

    testrun of ADC project
    not working

--- echo/Debug/main.d
+++ echo/Debug/main.d
@@ -3,15 +3,17 @@
 main.obj: ../main.c
 main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/stdbool.h
 main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/stdint.h
-main.obj: E:/github/BA2/.git/echo/driverlib/gpio.h
-main.obj: E:/github/BA2/.git/echo/driverlib/pin_map.h
-main.obj: E:/github/BA2/.git/echo/driverlib/sysctl.h
-main.obj: E:/github/BA2/.git/echo/inc/hw_memmap.h
+main.obj: ../inc/hw_memmap.h
+main.obj: ../driverlib/adc.h
+main.obj: ../driverlib/gpio.h
+main.obj: ../driverlib/pin_map.h
+main.obj: ../driverlib/sysctl.h
 
 ../main.c: 
 C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/stdbool.h: 
 C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/stdint.h: 
-E:/github/BA2/.git/echo/driverlib/gpio.h: 
-E:/github/BA2/.git/echo/driverlib/pin_map.h: 
-E:/github/BA2/.git/echo/driverlib/sysctl.h: 
-E:/github/BA2/.git/echo/inc/hw_memmap.h: 
+../inc/hw_memmap.h: 
+../driverlib/adc.h: 
+../driverlib/gpio.h: 
+../driverlib/pin_map.h: 
+../driverlib/sysctl.h: 
--- echo/main.c
+++ echo/main.c
@@ -1,9 +1,12 @@
-#include <stdbool.h> /*driverlib header requires stdbool.h to be included a first header file before any driverlib header*/
+#include <stdbool.h>
 #include <stdint.h>
-#include <driverlib/gpio.h>
-#include <driverlib/pin_map.h>/*supplies GPIO_PIN_x*/
-#include <driverlib/sysctl.h>
-#include <inc/hw_memmap.h>/*supplies GPIO_PORTx_BASE*/
+#include "inc/hw_memmap.h"
+#include "driverlib/adc.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+
+
 /*Controller is initially clocked with 16MHz*/
 #define F_CPU 16000000
 
@@ -11,27 +14,131 @@
  * main.c
  */
 void main(void) {
+#if defined(TARGET_IS_TM4C129_RA0) ||                                         \
+    defined(TARGET_IS_TM4C129_RA1) ||                                         \
+    defined(TARGET_IS_TM4C129_RA2)
+    uint32_t ui32SysClock;
+#endif
+
+    //
+    // This array is used for storing the data read from the ADC FIFO. It
+    // must be as large as the FIFO for the sequencer in use.  This example
+    // uses sequence 3 which has a FIFO depth of 1.  If another sequence
+    // was used with a deeper FIFO, then the array size must be changed.
+    //
+    uint32_t pui32ADC0Value[1];
+
+    //
+    // Set the clocking to run at 20 MHz (200 MHz / 10) using the PLL.  When
+    // using the ADC, you must either use the PLL or supply a 16 MHz clock
+    // source.
+    // TODO: The SYSCTL_XTAL_ value must be changed to match the value of the
+    // crystal on your board.
+    //
+#if defined(TARGET_IS_TM4C129_RA0) ||                                         \
+    defined(TARGET_IS_TM4C129_RA1) ||                                         \
+    defined(TARGET_IS_TM4C129_RA2)
+    ui32SysClock = sysclock = SysCtlClockFreqSet( SYSCTL_XTAL_25MHZ | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480, 10000000);
+#else
+    SysCtlClockSet(SYSCTL_SYSDIV_10 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
+                   SYSCTL_XTAL_16MHZ);
+#endif
     uint32_t ui32Strength;
     uint32_t ui32PinType;
-    uint8_t ui8val = 1;
-    volatile uint32_t ui32Loop = 0;
 
-    /*activate gpio port n*/
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOL);
 
-    /*configure GPIO_PIN_1 on port N as std pin*/
-    GPIOPadConfigGet(GPIO_PORTN_BASE, GPIO_PIN_1, &ui32Strength, &ui32PinType);
-    GPIOPadConfigSet(GPIO_PORTN_BASE, GPIO_PIN_1,ui32Strength,GPIO_PIN_TYPE_STD);
+    GPIOPadConfigGet(GPIO_PORTL_BASE, GPIO_PIN_3, &ui32Strength, &ui32PinType);
+    GPIOPadConfigSet(GPIO_PORTL_BASE, GPIO_PIN_3, ui32Strength, GPIO_PIN_TYPE_STD);
 
-    /*set pin 1 of gpio port n to output*/
-    GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE,GPIO_PIN_1);
+    GPIOPinTypeGPIOOutput(GPIO_PORTL_BASE,GPIO_PIN_3);
 
-	while(1)
-	{
-	    ui8val = ui8val ^ 2;/*toggle bit 1 in variable val*/
-        /*write value of val to pin 1 of port n*/
-        GPIOPinWrite (GPIO_PORTN_BASE, GPIO_PIN_1, 0);
-        // for(ui32Loop = 0; ui32Loop < 400000; ui32Loop++){}
-        SysCtlDelay(F_CPU/6);  //approx 0,5 s
-	}
+    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_3, 1);
+
+    //
+    // The ADC0 peripheral must be enabled for use.
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+
+    //
+    // For this example ADC0 is used with AIN0 on port E7.
+    // The actual port and pins used may be different on your part, consult
+    // the data sheet for more information.  GPIO port E needs to be enabled
+    // so these pins can be used.
+    // TODO: change this to whichever GPIO port you are using.
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+
+    //
+    // Select the analog ADC function for these pins.
+    // Consult the data sheet to see which functions are allocated per pin.
+    // TODO: change this to select the port/pin you are using.
+    //
+    GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_3);
+
+    //
+    // Enable sample sequence 3 with a processor signal trigger.  Sequence 3
+    // will do a single sample when the processor sends a signal to start the
+    // conversion.  Each ADC module has 4 programmable sequences, sequence 0
+    // to sequence 3.  This example is arbitrarily using sequence 3.
+    //
+    ADCSequenceConfigure(ADC0_BASE, 3, ADC_TRIGGER_PROCESSOR, 0);
+
+    //
+    // Configure step 0 on sequence 3.  Sample channel 0 (ADC_CTL_CH0) in
+    // single-ended mode (default) and configure the interrupt flag
+    // (ADC_CTL_IE) to be set when the sample is done.  Tell the ADC logic
+    // that this is the last conversion on sequence 3 (ADC_CTL_END).  Sequence
+    // 3 has only one programmable step.  Sequence 1 and 2 have 4 steps, and
+    // sequence 0 has 8 programmable steps.  Since we are only doing a single
+    // conversion using sequence 3 we will only configure step 0.  For more
+    // information on the ADC sequences and steps, reference the datasheet.
+    //
+    ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH0 | ADC_CTL_IE |
+                             ADC_CTL_END);
+
+    //
+    // Since sample sequence 3 is now configured, it must be enabled.
+    //
+    ADCSequenceEnable(ADC0_BASE, 3);
+
+    //
+    // Clear the interrupt status flag.  This is done to make sure the
+    // interrupt flag is cleared before we sample.
+    //
+    ADCIntClear(ADC0_BASE, 3);
+
+    while(1)
+    {
+        //
+        // Trigger the ADC conversion.
+        //
+        ADCProcessorTrigger(ADC0_BASE, 3);
+
+        //
+        // Wait for conversion to be completed.
+        //
+        while(!ADCIntStatus(ADC0_BASE, 3, false))
+        {
+        }
+
+        //
+        // Clear the ADC interrupt flag.
+        //
+        ADCIntClear(ADC0_BASE, 3);
+
+        //
+        // Read ADC Value.
+        //
+        ADCSequenceDataGet(ADC0_BASE, 3, pui32ADC0Value);
+
+        #if defined(TARGET_IS_TM4C129_RA0) ||                                         \
+            defined(TARGET_IS_TM4C129_RA1) ||                                         \
+            defined(TARGET_IS_TM4C129_RA2)
+                SysCtlDelay(ui32SysClock / 12);
+        #else
+                SysCtlDelay(SysCtlClockGet() / 12);
+        #endif
+    }
 }
